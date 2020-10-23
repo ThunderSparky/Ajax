174f77a606b2187d079272490ef4fe4a051782a4
StartBimbo <julinhovasquez20@gmail.com>
2020-10-23 01:32:15 -0500

    Primera llamada, usando AJAX

--- js/05.js
+++ js/05.js
@@ -9,18 +9,19 @@ function cargarContenido() {
     // abrimos la conexión
     xhr.open("GET", "texto_prueba.txt", true);
 
-    // revisar que cambie
+    // la funcion, se ejecutará cada vez que el método onreadystatechange cambie
     xhr.onreadystatechange = function() {
-        //console.log(xhr.readyState);
-
+        //console.log(xhr.readyState); //Esto es para ver el estado
+        //Con el metodo readyState, veamos el estado, si es 4 significa que todo esta correcto y status =200 es 
+        //cuando todo esta correcto
         if (xhr.readyState == 4 && xhr.status == 200) {
             //console.log("Se cargo correctamente");
-
             var contenido = document.getElementById('contenido');
+            //El metodo innerHTML sirve para cambiar el html, y por otro lado el metodo responseText
+            //retorna la respuesta como string (es como si convirtiera la respuesta en string)
             contenido.innerHTML = xhr.responseText;
         }
     };
-
     xhr.send();
 }
 
